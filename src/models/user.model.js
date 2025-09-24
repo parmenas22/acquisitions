dbc9c2999eae9bc5ab4f0c2380104e2cce7345c8
parmenas22 <parmenas.macharia@griffinglobaltech.com>
2025-09-24 17:05:22 +0300

    set up neon postgres with drizzle

---
+++ src/models/user.model.js
@@ -0,0 +1,11 @@
+import { pgTable, timestamp, serial, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  role: varchar("role", { length: 50 }).notNull().default("user"),
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
+});
