2b33b60677ef18c76639251ff8a8bd86cbf00acc
parmenas22 <parmenas.macharia@griffinglobaltech.com>
2025-09-25 20:05:21 +0300

    set up logging and middlewares

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import express from "express";
-import dotenv from "dotenv";
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-dotenv.config();
-const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.info(message.trim()),
+    },
+  })
+);
+
 app.get("/", (req, res) => {
+  logger.info("Hello from Acquisitions!!");
   res.status(200).send("Hello from acquisitions API!!");
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
-import { neon } from "drizzle-orm/neon-http";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle, neon } from "drizzle-orm/neon-http";
 
 const sql = neon(process.env.DATABASE_URL);
 
